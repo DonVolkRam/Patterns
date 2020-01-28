1b2ad57c8caa8d75a3b9600b357726ab865f7991
DonVolkRam <success.vka@gmail.com>
2020-01-28 15:12:33 +0300

    done second

---
+++ HomeTask2.1/HomeTask2.2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            var clothes = new Clothes();
+            var db = new List<IDiscountable>() { car, clothes };
+            foreach (var item in db)
+            {
+                item.SetDiscount(10);
+            }
+        }
+    }
+    interface IItem
+    {
+        void SetPrice(double price);
+    }
+    class Clothes : IItem, IClothable, IDiscountable
+    {
+        public void SetColor(Color color) { }
+        public void SetDiscount(double discount) { }
+        public void SetPrice(double price) { }
+        public void SetPromocode(string promocode) { }
+        public void SetSize(Size size) { }
+    }
+    interface IDiscountable
+    {
+        void SetDiscount(double discount);
+        void SetPromocode(string promocode);
+    }
+    interface IClothable
+    {
+        void SetColor(Color color);
+        void SetSize(Size size);
+    }
+
+    class Car: IItem, IDiscountable
+    {
+        public void SetDiscount(double discount) { }
+        public void SetPrice(double price) { }
+        public void SetPromocode(string promocode) { }
+    }
+
+    public class Size
+    {
+    }
+
+    public class Color
+    {
+    }
+}
