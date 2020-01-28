1926d13cac80f3f55de975e01b5b199ba7600f43
DonVolkRam <success.vka@gmail.com>
2020-01-28 14:55:40 +0300

    First Task

---
+++ HomeTask2.1/HomeTask2.1/IOrderSource.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeTask2._1
+{
+    public interface IOrderSource
+    {
+        Order Load(int orderId);
+        void Save(Order order);
+        void Update(Order order);
+        void Delete(Order order);
+    }
+}
